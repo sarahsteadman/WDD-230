39bda343030c5bf1ec1016aa444280d1bf757e98
sarahsteadman <fairyfaye4real@gmail.com>
2023-04-20 14:11:35 -0600

    added js

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+console.log("Hidey Ho!");
+document.querySelector("#year"). innerHTML = new Date().getFullYear();
+document.querySelector("#update"). innerHTML = document.lastModified;
+console.log("Hidey Ho!");
