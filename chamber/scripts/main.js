829b1b243b55d60c8b58097cc64c28e2697fefa0
sarahsteadman <fairyfaye4real@gmail.com>
2023-05-10 00:14:15 -0600

    made chamber of commerce site

---
+++ chamber/scripts/main.js
@@ -0,0 +1,17 @@
+
+const datefield = document.querySelector("#time");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now
+    );
+    datefield.textContent = fulldate;
+
+const hamButton = document.querySelector('#hamButton');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('responsive');
+});
+    
+document.querySelector("#year"). innerHTML = new Date().getFullYear();
+document.querySelector("#update"). innerHTML = document.lastModified;
