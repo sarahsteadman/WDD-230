068b94987a7911d5a6b3ef36f8e19576f1560ccd
sarahsteadman <fairyfaye4real@gmail.com>
2023-05-12 23:11:50 -0600

    more stuff

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -7,10 +7,11 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
     datefield.textContent = fulldate;
 
 const hamButton = document.querySelector('#hamButton');
-const navigation = document.querySelector('.navigation');
+const navigation = document.querySelector('nav');
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('responsive');
+    console.log("I hear you!");
 });
     
 document.querySelector("#year").innerHTML = new Date().getFullYear();
