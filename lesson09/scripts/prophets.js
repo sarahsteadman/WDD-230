5dbbe3e076f51872ec34cfb8c017b15a6b5205d1
sarahsteadman <fairyfaye4real@gmail.com>
2023-06-13 00:39:40 -0600

    no log

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -31,7 +31,7 @@ const displayProphets = (prophets) =>{
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets); 
+    // console.table(data.prophets); 
     displayProphets(data.prophets);
   }
   
