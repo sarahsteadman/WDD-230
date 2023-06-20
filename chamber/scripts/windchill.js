5ea23b921f09d38ba06d73d59f06f020d60d16df
sarahsteadman <fairyfaye4real@gmail.com>
2023-06-20 17:41:50 -0600

    Api for chamber weather done

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,11 +1,47 @@
-let speed = document.querySelector("#speed").innerHTML;
-let temperature = document.querySelector("#temperature").innerHTML;
+const url =
+"https://api.openweathermap.org/data/2.5/weather?lat=43.7314&lon=7.419&units=imperial&appid=b500430b0da641eda0c3a8a5dea9ca0e";
 
-if(temperature <= 50 && speed > 3){
-let windchill = 35.74 + (.6215 * temperature) - (35.75 * (speed ** .16)) + (.4275 * temperature * (speed ** .16));
-document.querySelector("#chill").innerHTML = Math.round(windchill);
+async function apiFetch() {
+    try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
-else{
-let   windchill = "N/A";
-document.querySelector("#chill").innerHTML = Math.round(windchill);
+function  displayResults(weatherData) {
+    temperature = weatherData.main.temp;
+    speed = weatherData.wind.speed;
+    calculateWindChill(speed,temperature);
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    
+    weatherIcon = document.querySelector("#icon");
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    document.querySelector("#iconCap").innerHTML = desc;
+  }
+
+function calculateWindChill(speed, temperature) {
+    document.querySelector("#speed").innerHTML = speed;
+    document.querySelector("#temperature").innerHTML = temperature;
+
+  if (temperature <= 50 && speed > 3) {
+    let windchill =
+      35.74 +
+      0.6215 * temperature -
+      35.75 * speed ** 0.16 +
+      0.4275 * temperature * speed ** 0.16;
+    document.querySelector("#chill").innerHTML = Math.round(windchill);
+  } else {
+    let windchill = "N/A";
+    document.querySelector("#chill").innerHTML = Math.round(windchill);
+  }
 }
+apiFetch();
--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -1,32 +1,35 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-
+// //////////////////////////////////////////////////////////////////////////////////
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=64.8378&lon=-147.7164&units=imperial&appid=b500430b0da641eda0c3a8a5dea9ca0e';
 
+// //////////////////////////////////////////////////////////////////////////////////
 async function apiFetch() {
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        displayResults(data)
-
-      } else {
-          throw Error(await response.text());
-      }
-    } catch (error) {
-        console.log(error);
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data)
+      
+    } else {
+      throw Error(await response.text());
     }
+  } catch (error) {
+    console.log(error);
   }
+  // //////////////////////////////////////////////////////////////////////////////////
+}
+
+function  displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
   
-  function  displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
   
-    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-    const desc = weatherData.weather[0].description;
-  
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc;
-  }
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+}
+// //////////////////////////////////////////////////////////////////////////////////
   apiFetch();
